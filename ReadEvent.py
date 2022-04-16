e6430301f64fc84c932d91018cbfe34480aeaca1
shanacon <n107502530@office365.ncu.edu.tw>
2022-04-16 20:53:31 +0800

    complete txt decode and send convert txt back

---
+++ ReadEvent.py
@@ -0,0 +1,10 @@
+import chardet
+
+def ReadData(path):
+    text = open(path, 'rb').read()
+    if chardet.detect(text)["encoding"] == 'GB2312':
+        LoadF = open(path, "r" , encoding = 'gb18030')
+    else :
+        LoadF = open(path, "r" , encoding = chardet.detect(text)["encoding"])
+    data =  LoadF.readlines()
+    return data
